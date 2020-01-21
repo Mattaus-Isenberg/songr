945a5df30d41df304e25ebd734cde2bdb418e1a8
Mattäus Sebastian Isenberg <mattaus@gmx.com>
2020-01-21 14:56:52 -0800

    implemented setters and test for it

--- src/main/java/com/echokinetic/songr/Album.java
+++ src/main/java/com/echokinetic/songr/Album.java
@@ -17,23 +17,53 @@ public class Album
         this.imageUrl = imageUrl;
     }
 
-    public String getTitle() {
+    public String getTitle()
+    {
         return title;
     }
 
-    public String getArtist() {
+    public String getArtist()
+    {
         return artist;
     }
 
-    public int getSongCount() {
+    public int getSongCount()
+    {
         return songCount;
     }
 
-    public int getLength() {
+    public int getLength()
+    {
         return length;
     }
 
-    public String getImageUrl() {
+    public String getImageUrl()
+    {
         return imageUrl;
+    }
+
+    public void setTitle(String title)
+    {
+        this.title = title;
+    }
+
+    public void setArtist(String artist)
+    {
+        this.artist = artist;
+    }
+
+    public void setSongCount(int songCount)
+    {
+        this.songCount = songCount;
+    }
+
+    public void setLength(int length)
+    {
+        this.length = length;
+    }
+
+    public void setImageUrl(String imageUrl)
+    {
+        this.imageUrl = imageUrl;
     }
 }
